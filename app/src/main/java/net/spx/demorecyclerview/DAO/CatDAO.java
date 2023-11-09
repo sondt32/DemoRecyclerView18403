252d9ea016b02f1c74085a6d26471d072d9bc7d5
snvn1511 <spxedu@gmail.com>
2023-11-09 08:49:01 +0700

    Initial commit

---
+++ app/src/main/java/net/spx/demorecyclerview/DAO/CatDAO.java
@@ -0,0 +1,59 @@
+package net.spx.demorecyclerview.DAO;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+
+import net.spx.demorecyclerview.DTO.CatDTO;
+import net.spx.demorecyclerview.DbHelper.MyDbHelper;
+
+import java.util.ArrayList;
+
+public class CatDAO {
+    MyDbHelper dbHelper;
+    SQLiteDatabase db;
+
+    public CatDAO(Context ct){
+        dbHelper = new MyDbHelper( ct ) ;
+        db = dbHelper.getWritableDatabase();
+    }
+
+    // hàm lấy danh sách thê loại
+    public ArrayList<CatDTO> getList(){
+
+        ArrayList<CatDTO> listCat = new ArrayList<>();
+
+        Cursor c = db.rawQuery("SELECT * FROM tb_cat ORDER BY name ASC ", null);
+        if(c.getCount()>0){
+            c.moveToFirst(); // đưa trỏ về đầu dòng
+            do {
+                CatDTO objCat = new CatDTO();
+                objCat.setId(     c.getInt(0)     );  // cách khác:  c.getInt( c.getColumnIndex("id"))
+                objCat.setName(   c.getString(1)  );
+
+                // cho đối tượng vào danh sách
+                listCat.add( objCat );
+
+            }while (c.moveToNext());
+        }else{
+            Log.d("zzzzzzzzzzzz", "getList: Không có dữ liệu");
+        }
+        return  listCat;
+    }
+    // hàm thêm mới
+    public int addRow(CatDTO objCat){
+        ContentValues values = new ContentValues();
+        values.put("name", objCat.getName() );
+        long kq = db.insert("tb_cat",null, values );
+        return  (int) kq; // nêu kq > 0 thì là thêm thành công và id là kq.
+        // yêu cầu id của bảng phải là autoincrement
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/net/spx/demorecyclerview/DbHelper/MyDbHelper.java
@@ -0,0 +1,45 @@
+package net.spx.demorecyclerview.DbHelper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDbHelper extends SQLiteOpenHelper {
+    static  String DB_NAME = "ql_banhang";
+    static  int DB_VERSION = 1;
+
+
+    public MyDbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // câu lệnh tạo bảng viết ở đây
+        // tạo bảng thê loại:
+        String sql_cat = "CREATE TABLE tb_cat ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE NOT NULL );";
+
+        sqLiteDatabase.execSQL( sql_cat ); // tạo xong bảng tb_cat
+
+        // tạo bảng sản phẩm
+        String sql_product = "CREATE TABLE tb_product ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, price INTEGER DEFAULT (0) NOT NULL,id_cat INTEGER NOT NULL CONSTRAINT fk_pro_cat REFERENCES tb_cat (id) );";
+
+        sqLiteDatabase.execSQL( sql_product );
+
+        //======== Thêm dòng dữ liệu ======
+        String insert_cat = "INSERT INTO tb_cat (name) VALUES ('Tivi'), ('Tủ lạnh'), ('Điều hòa') ";
+        sqLiteDatabase.execSQL( insert_cat );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // cập nhật
+        if(i<i1){
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS  tb_product");
+            // tb_product là bảng bên nhiều phải xóa trước
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS  tb_cat");
+
+        }
+    }
+}
