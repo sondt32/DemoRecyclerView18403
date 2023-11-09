b2aa2d58098566b638849272e8701e9264e370c6
snvn1511 <spxedu@gmail.com>
2023-11-09 09:12:58 +0700

    Xong adapter buoc 2

--- app/src/main/java/net/spx/demorecyclerview/Adapter/CatAdapter.java
+++ app/src/main/java/net/spx/demorecyclerview/Adapter/CatAdapter.java
@@ -1,6 +1,8 @@
 package net.spx.demorecyclerview.Adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -25,17 +27,23 @@ public class CatAdapter extends RecyclerView.Adapter<CatAdapter.CatViewHolder> {
     @NonNull
     @Override
     public CatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //return null;
+        LayoutInflater inflater = ((Activity) context ).getLayoutInflater();
+        View v = inflater.inflate(R.layout.row_cat, parent, false);
+        return  new CatViewHolder(v );
     }
 
     @Override
     public void onBindViewHolder(@NonNull CatViewHolder holder, int position) {
-
+        // gắn dữ liệu vào giao diện:
+        CatDTO objCat = listCat.get(position);
+        holder.tv_id.setText(  objCat.getId() + ""   );// chuyển về chuỗi với ữ liệu so
+        holder.tv_name.setText( objCat.getName() );
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listCat.size() ;  // không quên return size
     }
 
     class CatViewHolder extends RecyclerView.ViewHolder{
