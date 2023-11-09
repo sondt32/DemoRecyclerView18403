d80af045c79380846935b6efee362e7a707e037b
snvn1511 <spxedu@gmail.com>
2023-11-09 09:16:35 +0700

    Tạo adapter để hiển thị recyclerview

--- app/src/main/java/net/spx/demorecyclerview/MainActivity.java
+++ app/src/main/java/net/spx/demorecyclerview/MainActivity.java
@@ -1,14 +1,35 @@
 package net.spx.demorecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import net.spx.demorecyclerview.Adapter.CatAdapter;
+import net.spx.demorecyclerview.DAO.CatDAO;
+import net.spx.demorecyclerview.DTO.CatDTO;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<CatDTO> listCat;
+    CatDAO dao;
+    RecyclerView rc_cat;
+    CatAdapter catAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dao = new CatDAO(this);
+        listCat = dao.getList();
+
+        rc_cat = findViewById(R.id.rc_cat);
+
+        catAdapter = new CatAdapter(this, listCat);
+
+        rc_cat.setAdapter( catAdapter );
+
     }
 }
